e98ed6916ddcc1e9dc2ef7d4c2d2f16f2b2bd701
Shanaia Chen <chenso@student.unimelb.edu.au>
2023-03-24 11:16:43 +1100

    added test2

--- search/program.py
+++ search/program.py
@@ -30,19 +30,30 @@ def search(input: dict[tuple, tuple]) -> list[tuple]:
 
     # Convert dictionary to list
     for item in input.items():
-        board.append(item)
+        # board.append(item)
         if (item[1][0] == 'r'):
             red.append(item)
+
+    # Space-Time Trade-Off
+    # It is more efficient to use a dictionary for lookup of elements because it takes less time to traverse in the dictionary than a list.
+    # Checking for membership of a value in a set (or dict, for keys) is blazingly fast (taking about a constant, short time), while in a list it takes time proportional to the list's length in the average and worst cases
+    # Maybe we don't have to convert dictionary to list???
 
     # MIGHT BE HELPFUL FOR DISTANCE/SPREAD CHECKING
     # You can look up the current occupancy/power of a cell using a coordinate tuple (r, q) as a key.
     # Just keep in mind that not all cells are necessarily occupied (the dictionary is a sparse representation), so check that the key exists before using it.
 
     for token in red:
-        solution = bfs(token, board)
-        solutions.append(solution.qsize, solution)
+        board = dict(input)
+        print(token)
+        print(board)
+        # solution = bfs(token, board)
+        # print(solution)
+        # print(solution.qsize)
+        # solutions.append(solution.qsize, solution)
 
-    return solutions.get()
+    # print(solutions.get())
+    return  # solutions.get()
 
     # Here we're returning "hardcoded" actions for the given test.csv file.
     # Of course, you'll need to replace this with an actual solution...
