e6b5ca68c9d9cf81e167d4396413e88fa3de0cce
vockoi <victoria.halim3@gmail.com>
2023-04-03 13:13:24 +1000

    update

--- search/program.py
+++ search/program.py
@@ -6,6 +6,7 @@ from .bfsHelpers import *
 from .ids import *
 from .astar_mega_relaxed import *
 
+
 def search(input: dict[tuple, tuple]) -> list[tuple]:
     """
     This is the entry point for your submission. The input is a dictionary
@@ -19,11 +20,11 @@ def search(input: dict[tuple, tuple]) -> list[tuple]:
     # The render_board function is useful for debugging -- it will print out a
     # board state in a human-readable format. Try changing the ansi argument
     # to True to see a colour-coded version (if your terminal supports it).
-    #print(render_board(input, ansi=True))
+    print(render_board(input, ansi=True))
 
     moves_made = astar_search(input)
 
-    #print(render_board(input, ansi=True))
+    # print(render_board(input, ansi=True))
 
     # Space-Time Trade-Off
     # It is more efficient to use a dictionary for lookup of elements because it takes less time to traverse in the dictionary than a list.
@@ -35,7 +36,7 @@ def search(input: dict[tuple, tuple]) -> list[tuple]:
     # Just keep in mind that not all cells are necessarily occupied (the dictionary is a sparse representation), so check that the key exists before using it.
 
     # print(solutions.get())
-    return moves_made # solutions.get()
+    return moves_made  # solutions.get()
 
     # Here we're returning "hardcoded" actions for the given test.csv file.
     # Of course, you'll need to replace this with an actual solution...
